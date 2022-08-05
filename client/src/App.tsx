385a5d66749c1530c2b7f4febf0361be1a538742
Waleed Butt <waleed3072@gmail.com>
2022-08-05 07:55:15 +0500

    search bar done

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,9 +1,44 @@
-import React from 'react';
+import React from "react";
+import { AiOutlineSearch } from "react-icons/ai";
 
 function App() {
+  const [focus, setFocus] = React.useState<boolean>(false);
+  const [search, setSearch] = React.useState<string>();
+
   return (
-    <div className="w-full h-full flex justify-center items-center">
-      Hello World!
+    <div className="w-screen h-screen flex flex-col">
+      <div
+        className={`w-full ${
+          focus || search ? "h-1/6" : "h-full"
+        } flex items-center justify-center transition-all duration-300`}
+      >
+        <div className={`flex w-8/12 items-center rounded-xl shadow-xl drop-shadow-xl px-2 border-2 ${focus ? "border-blue-500" : "border-transparent"}`}>
+          <AiOutlineSearch className="text-xl xs:text-4xl" />
+          <input
+            value={search}
+            onChange={(e) => {
+              e.preventDefault();
+              setSearch(e.target.value);
+            }}
+            onFocus={(e) => {
+              e.preventDefault();
+              setFocus(true);
+            }}
+            onBlur={(e) => {
+              e.preventDefault();
+              setFocus(false);
+            }}
+            className={`px-2 py-5 border-0 focus:outline-0 bg-transparent text-lg`}
+          />
+        </div>
+      </div>
+      <div
+        className={`w-full ${
+          focus || search ? "h-5/6" : "h-0"
+        } bg-red-500 transition-all duration-300`}
+      >
+
+      </div>
     </div>
   );
 }
--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -2,7 +2,11 @@
 module.exports = {
   content: ["./src/**/*.{html,js,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      screens: {
+        xs: "320px"
+      }
+    },
   },
   plugins: [],
 }
